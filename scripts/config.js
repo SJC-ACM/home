fd5dbba114282984998dafc38cc92d7693d22105
TheWolfBadger <TheWolfBadger@gmail.com>
2018-10-09 21:59:58 -0400

    - Added "Join Now" div section - Navbar buttons now work with smooth scrollTos (cool) - Got "Officers" div section ready to be styled - Filled in "About Us" div paragraph - Added dog-at-computer image (png) - Added sjc-btc-building image (png)

--- scripts/config.js
+++ scripts/config.js
@@ -5,7 +5,11 @@ var clubImage = "css/img/club-in-action.jpg";
 
 var quote = "\"Here at the St. Joseph's ACM club we love to hang out, eat pizza, play video games, as well as get to know others within our field!\" <br />- Jared Scarito";
 
-var about_us = "";
+var about_us =
+"The ACM Club is the Computer Science club at St. Joseph's College. We have a driven focus to expand our understanding of the computing disciples " +
+"while helping others learn along the way. Our club actively participates in the computing world by holding workshops, inviting guest speakers from " +
+"the field to talk about their careers, as well as holding video game tournaments! Our club meetings usually take place every Wednesday at common " +
+"hour. Feel free to stop by to casually chill with others in the field as well as play some PS4 \"Disc Jam\".";
 
 var advisorPicture = "";
 var advisorName = "";
@@ -13,16 +17,18 @@ var advisorName = "";
 var presidentPicture = "css/img/president.png";
 var presidentName = "Kerri McMahon";
 
-var vicePresidentPicture = "";
-var vicePresidentName = "";
+var vicePresidentPicture = "css/img/vice-president.png";
+var vicePresidentName = "Kyle Waldron";
 
-var secretaryPicture = "";
-var secretaryName = "";
+var secretaryPicture = "css/img/secretary.png";
+var secretaryName = "Josh Papello";
 
-var treasurerPicture = "";
-var tresurerName = "";
+var treasurerPicture = "css/img/treasurer.png";
+var tresurerName = "Alana Caporale";
 
-var joinNowEmail = "";
+var joinNowEmail = "acm.li@student.sjcny.edu";
+
+var discordLink = "";
 
 
 /**
